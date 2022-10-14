32067e8bd0ad8a522af088c5b004a0558046f7d9
happyfrog <rainbow610@naver.com>
2022-10-14 21:51:36 +0900

    서버쪽 파일 다운로드 테스트

--- src/main/java/com/happy/happyfrog/Controller/UploadController.java
+++ src/main/java/com/happy/happyfrog/Controller/UploadController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import java.io.File;
+
 @Controller
 public class UploadController {
+    private String path = "C:\\filedown";
 
-//    톰캣 에러 수정 중
     @GetMapping("/write")
     public String write(){
         return "write";
@@ -19,11 +21,25 @@ public class UploadController {
 
     @PostMapping("/write")
     public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file){
-        System.out.println("file = " + file);
-        if(file != null){
-            return new ResponseEntity<>("UPLOAD_OK", HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>("UPLOAD_FAIL", HttpStatus.BAD_REQUEST);
+        try {
+            String originFilename = file.getOriginalFilename();
+            String extName = originFilename.substring(originFilename.lastIndexOf("."),originFilename.length());
+            long size = file.getSize();
+
+
+            System.out.println("originFilename : " + originFilename);
+            System.out.println("extensionName : " + extName);
+            System.out.println("size : " + size);
+
+            if(!file.isEmpty()){
+                File downFile = new File(path,originFilename);
+                file.transferTo(downFile);
+            }else{
+                return new ResponseEntity<>("UPLOAD_FAIL", HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return new ResponseEntity<>("UPLOAD_OK", HttpStatus.OK);
     }
 }
